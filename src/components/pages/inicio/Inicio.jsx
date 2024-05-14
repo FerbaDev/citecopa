1a020da0632c6513d61be2ac91c0ea503db6dd42
FerbaDev <elferbarron1@gmail.com>
2024-05-14 02:26:47 -0300

    se carga seccion taller fondo naranja

--- src/components/pages/inicio/Inicio.jsx
+++ src/components/pages/inicio/Inicio.jsx
@@ -4,12 +4,14 @@ import "./inicio.css"
 import "../../../index.css";
 import ClubSeccion from "./ClubSeccion";
 import LabSeccion from "./LabSeccion";
+import { TallerSeccion } from "./TallerSeccion";
 
 export const Inicio = () => {
   return (
     <Box  >
       <ClubSeccion />
       <LabSeccion />
+      <TallerSeccion />
     </Box>
   );
 };
---
+++ src/components/pages/inicio/TallerSeccion.jsx
@@ -0,0 +1,7 @@
+
+
+export const TallerSeccion = () => {
+  return (
+    <div className="seccion-taller">TallerSeccion</div>
+  )
+}
