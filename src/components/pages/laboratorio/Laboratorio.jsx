a8b7079d88baa894a7d444682558daf04900d0e5
FerbaDev <elferbarron1@gmail.com>
2024-05-30 20:06:54 -0300

    probando fondo en deploy

--- src/components/pages/laboratorio/Laboratorio.jsx
+++ src/components/pages/laboratorio/Laboratorio.jsx
@@ -1,15 +1,12 @@
+import { SeccionLab } from "./SeccionLab"
+import { SeccionTaller } from "./SeccionTaller"
 import "./laboratorio.css"
 
 export const Laboratorio = () => {
   return (
-    <div className="lab-hero-container">
-      <div className="lab-hero-titles">
-        <h1 className="neon-text-white">Laboratorio de proyectos digitales</h1>
-        <h3 className="neon-text-pink">Diseño 3d, Apps móviles y web, Gaming y Multimedia</h3>
-      </div>
-      <div className="lab-hero-images">
-        <img src="./javascript.svg" alt="" />
-      </div>
+    <div>
+      <SeccionLab />
+      <SeccionTaller />
     </div>
   )
 }
---
+++ src/components/pages/laboratorio/SeccionTaller.jsx
@@ -0,0 +1,12 @@
+
+
+export const SeccionTaller = () => {
+  return (
+    <div className="taller-container">
+        <div className="taller-title">
+            <h2>Taller de construcciones digitales</h2>
+        </div>
+        <div></div>
+    </div>
+  )
+}
