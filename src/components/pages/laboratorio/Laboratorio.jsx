c68869db62bef8bf16edaa9ac8c279a3a4f8cc59
FerbaDev <elferbarron1@gmail.com>
2024-05-31 19:27:52 -0300

    se trabaja en taller y comarca

--- src/components/pages/laboratorio/Laboratorio.jsx
+++ src/components/pages/laboratorio/Laboratorio.jsx
@@ -1,12 +1,14 @@
-import { SeccionLab } from "./SeccionLab"
-import { SeccionTaller } from "./SeccionTaller"
-import "./laboratorio.css"
+import SeccionComarca from "./SeccionComarca";
+import { SeccionLab } from "./SeccionLab";
+import { SeccionTaller } from "./SeccionTaller";
+import "./laboratorio.css";
 
 export const Laboratorio = () => {
   return (
     <div>
       <SeccionLab />
       <SeccionTaller />
+      <SeccionComarca />
     </div>
-  )
-}
+  );
+};
---
+++ src/components/pages/laboratorio/SeccionComarca.jsx
@@ -0,0 +1,35 @@
+const SeccionComarca = () => {
+  return (
+    <div className="comarca-container">
+      <h2 className="neon-text-white">Comarca Tech</h2>
+      <div className="taller-info">
+        <div className="comarca-info-item">
+          <p>Desafios de diseño</p>
+          <a href="" className="btn">
+            Ver más
+          </a>
+        </div>
+        <div className="comarca-info-item">
+          <p>Hackatones sostenibles</p>
+          <a href="" className="btn">
+            Ver más
+          </a>
+        </div>
+        <div className="comarca-info-item">
+          <p>Gaming y Desarrollos</p>
+          <a href="" className="btn">
+            Ver más
+          </a>
+        </div>
+        <div className="comarca-info-item">
+          <p>Expos y charlas</p>
+          <a href="" className="btn">
+            Ver más
+          </a>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default SeccionComarca;
