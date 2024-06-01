b7112e62136d3faacf7e0625843f56326a2245af
FerbaDev <elferbarron1@gmail.com>
2024-06-01 01:16:12 -0300

    se trabaja en comarca tech

--- src/components/pages/laboratorio/SeccionComarca.jsx
+++ src/components/pages/laboratorio/SeccionComarca.jsx
@@ -1,29 +1,31 @@
 const SeccionComarca = () => {
   return (
     <div className="comarca-container">
-      <h2 className="neon-text-white">Comarca Tech</h2>
-      <div className="taller-info">
+      <div className="comarca-title-container">
+        <h2 className="neon-text-white">Comarca Tech</h2>
+      </div>
+      <div className="comarca-info">
         <div className="comarca-info-item">
           <p>Desafios de diseño</p>
-          <a href="" className="btn">
+          <a href="#" className="btn">
             Ver más
           </a>
         </div>
         <div className="comarca-info-item">
           <p>Hackatones sostenibles</p>
-          <a href="" className="btn">
+          <a href="#" className="btn">
             Ver más
           </a>
         </div>
         <div className="comarca-info-item">
           <p>Gaming y Desarrollos</p>
-          <a href="" className="btn">
+          <a href="#" className="btn">
             Ver más
           </a>
         </div>
         <div className="comarca-info-item">
           <p>Expos y charlas</p>
-          <a href="" className="btn">
+          <a href="#" className="btn">
             Ver más
           </a>
         </div>
