f75c928a6aa86ade5c5d93ee71b7d60cb444a0ab
FerbaDev <elferbarron1@gmail.com>
2024-06-02 13:54:22 -0300

    se trabaja en seccion proyectos

--- src/components/pages/laboratorio/SeccionProyectos.jsx
+++ src/components/pages/laboratorio/SeccionProyectos.jsx
@@ -4,7 +4,20 @@ const SeccionProyectos = () => {
   return (
     <div className="proyectos-container">
         <h2 className="neon-text-white">Proyectos comunitarios</h2>
-
+        <div className="proyectos-cards-container">
+          <div className="proyectos-card">
+            <h3>Inclusión digital</h3>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. At cum id alias corporis tenetur ducimus!</p>
+          </div>
+          <div className="proyectos-card">
+            <h3>Tecnologías sociales</h3>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. At cum id alias corporis tenetur ducimus!</p>
+          </div>
+          <div className="proyectos-card">
+            <h3>Ciencia ciudadana</h3>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. At cum id alias corporis tenetur ducimus!</p>
+          </div>
+        </div>
     </div>
   )
 }
