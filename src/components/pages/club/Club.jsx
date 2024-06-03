2de1b65c77565256726e2dec7ad6ec40bde6d21d
FerbaDev <elferbarron1@gmail.com>
2024-06-02 23:21:00 -0300

    se corrigen fuentes y se trabaja en club

--- src/components/pages/club/Club.jsx
+++ src/components/pages/club/Club.jsx
@@ -3,7 +3,11 @@ import "./club.css"
 export const Club = () => {
   return (
     <div className="club-container">
-     <h1>hola</h1>
+      <h1>Club de ciencias</h1>
+      <div>
+        <img src="/man-scientist.svg" alt="" />
+        <img src="/scientist.svg" alt="" />
+     </div>
     </div>
   );
 };
