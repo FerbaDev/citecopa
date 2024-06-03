13215766ef9af9ebd1df0b5dc4f0feb8b06b900a
FerbaDev <elferbarron1@gmail.com>
2024-06-03 02:20:52 -0300

    se trabaja en seccion club

--- src/components/pages/club/Club.jsx
+++ src/components/pages/club/Club.jsx
@@ -1,13 +1,43 @@
 import "./club.css"
+import ArrowCircleRightOutlinedIcon from '@mui/icons-material/ArrowCircleRightOutlined';
 
 export const Club = () => {
   return (
     <div className="club-container">
-      <h1>Club de ciencias</h1>
-      <div>
-        <img src="/man-scientist.svg" alt="" />
-        <img src="/scientist.svg" alt="" />
-     </div>
+      <div className="club-header">
+        <h1>Club de ciencias</h1>
+        <div className="club-img-header">
+          <img src="/man-scientist.svg" alt="" />
+          <img src="/scientist.svg" alt="" />
+        </div>
+      </div>
+      <div className="main-club">
+      <div className="cta-club-container">
+        <div className="card-club-cta">
+          <img src="/anotate.svg" alt="" />
+          <h2>Anotate!</h2>
+          <a href="#"><ArrowCircleRightOutlinedIcon sx={{fontSize:"3em", color: "#212121"}}/></a>
+        </div>
+      </div>
+      <div className="club-cards-container">
+        <div className="club-card">
+          <img src="/creatilab.svg" alt="" />
+          <p>Creatilab</p>
+        </div>
+        <div className="club-card">
+          <img src="/microscopio.svg" alt="" />
+          <p>Explorá ciencias</p>
+        </div>
+        <div className="club-card">
+          <img src="/tecnoarte.svg" alt="" />
+          <p>Tecnoarte</p>
+        </div>
+        <div className="club-card">
+          <img src="/red-de-clubes.svg" alt="" />
+          <p>Red de clubes</p>
+        </div>
+      </div>
+      </div>
     </div>
   );
 };
