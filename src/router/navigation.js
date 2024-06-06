df31425002b55a245c8d910ded610ceccfc5e4b8
FerbaDev <elferbarron1@gmail.com>
2024-06-06 17:51:07 -0300

    se trabaja en navbar

--- src/router/navigation.js
+++ src/router/navigation.js
@@ -1,26 +1,31 @@
 import HomeIcon from "@mui/icons-material/Home";
-
-
-
+import BiotechIcon from "@mui/icons-material/Biotech";
+import GroupsIcon from "@mui/icons-material/Groups";
+import Diversity3Icon from "@mui/icons-material/Diversity3";
 
 export const menuItems = [
+  {
+    id: "home",
+    path: "/",
+    title: "Home",
+    Icon: HomeIcon,
+  },
   {
     id: "club",
     path: "/club",
     title: "Club",
-    Icon: HomeIcon,
+    Icon: GroupsIcon,
   },
   {
     id: "laboratorio",
     path: "/laboratorio",
     title: "Laboratorio",
-    Icon: HomeIcon,
+    Icon: BiotechIcon,
   },
   {
     id: "comunidad",
     path: "/comunidad",
     title: "Comunidad",
-    Icon: HomeIcon,
-  }
-  
+    Icon: Diversity3Icon,
+  },
 ];
