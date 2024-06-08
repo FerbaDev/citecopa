5f63e8839a119d26ae7e6a344bab29eea31c6cdd
FerbaDev <elferbarron1@gmail.com>
2024-06-08 16:52:09 -0300

    se agregan iconos a navbar

--- src/router/navigation.js
+++ src/router/navigation.js
@@ -2,6 +2,11 @@ import HomeIcon from "@mui/icons-material/Home";
 import BiotechIcon from "@mui/icons-material/Biotech";
 import GroupsIcon from "@mui/icons-material/Groups";
 import Diversity3Icon from "@mui/icons-material/Diversity3";
+import ConstructionIcon from '@mui/icons-material/Construction';
+import MemoryIcon from '@mui/icons-material/Memory';
+import SmartToyIcon from '@mui/icons-material/SmartToy';
+import FestivalIcon from '@mui/icons-material/Festival';
+import Diversity2Icon from '@mui/icons-material/Diversity2';
 
 export const menuItems = [
   {
@@ -21,6 +26,36 @@ export const menuItems = [
     path: "/laboratorio",
     title: "Laboratorio",
     Icon: BiotechIcon,
+  },
+  {
+    id: "taller",
+    path: "/taller",
+    title: "Taller",
+    Icon: ConstructionIcon,
+  },
+  {
+    id: "comarca",
+    path: "/comarcatech",
+    title: "Comarca Tech",
+    Icon: MemoryIcon,
+  },
+  {
+    id: "liga",
+    path: "/liga-robotica",
+    title: "Liga Robótica",
+    Icon: SmartToyIcon,
+  },
+  {
+    id: "feria",
+    path: "/feria-de-ciencias",
+    title: "Feria de ciencias",
+    Icon: FestivalIcon,
+  },
+  {
+    id: "proyectos",
+    path: "/proyectos-comunitarios",
+    title: "Proyectos comunitarios",
+    Icon: Diversity2Icon,
   },
   {
     id: "comunidad",
