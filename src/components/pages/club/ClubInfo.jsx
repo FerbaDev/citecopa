d44df0cb748002ce4858cd6a4c6e9ab92ded06d8
FerbaDev <elferbarron1@gmail.com>
2024-06-14 00:04:15 -0300

    se trabaja en club

--- src/components/pages/club/ClubInfo.jsx
+++ src/components/pages/club/ClubInfo.jsx
@@ -34,45 +34,55 @@ const ClubInfo = () => {
             </header>
 
             <section className={styles.section} id='explora-ciencias'>
-                <h2>Explora Ciencias</h2>
-                <p>
-                    Podrás aprender sobre <strong>ciencias naturales, conservación de especies, 
-                    biodiversidad digital, biotecnologías, astronomía y ciencias exactas (física, química, matemáticas)</strong>. 
-                    Descubre los <strong>ecosistemas y acciones ambientales</strong> y explora las <strong>historias de las ciencias </strong> 
-                    de manera divertida e interactiva.
-                </p>
+                <div className={styles.sectionInfo}>
+                    <h2>Explora Ciencias</h2>
+                    <p>
+                        Podrás aprender sobre <strong>ciencias naturales, conservación de especies, 
+                        biodiversidad digital, biotecnologías, astronomía y ciencias exactas (física, química, matemáticas)</strong>. 
+                        Descubre los <strong>ecosistemas y acciones ambientales</strong> y explora las <strong>historias de las ciencias </strong> 
+                        de manera divertida e interactiva.
+                    </p>
+                </div>
                 <img src="/img-club-de-ciencias1.webp" alt="Explora Ciencias" className={styles.image}/>
             </section>
 
-            <section className={styles.section} id='creatilab'>
-                <h2>CreatiLab</h2>
-                <p>
-                    En el <strong>CreatiLab</strong>, podrás experimentar con <strong>inteligencia artificial, realidad virtual y aumentada, 
-                    gaming y experimentos de ciencia viva</strong>. Construye <strong>artilugios tecnológicos</strong> y participa en 
-                    emocionantes <strong>construcciones</strong> que te harán sentir como un verdadero científico.
-                </p>
+            <section className={styles.section2} id='creatilab'>
+                <div className={styles.sectionInfo}>
+                    <h2>CreatiLab</h2>
+                    <p>
+                        En el <strong>CreatiLab</strong>, podrás experimentar con <strong>inteligencia artificial, realidad virtual y aumentada, 
+                        gaming y experimentos de ciencia viva</strong>. Construye <strong>artilugios tecnológicos</strong> y participa en 
+                        emocionantes <strong>construcciones</strong> que te harán sentir como un verdadero científico.
+                    </p>
+                </div>
                 <img src="/creatilab2.webp" alt="CreatiLab" className={styles.image}/>
             </section>
 
             <section className={styles.section} id='tecnoarte'>
-                <h2>Innova y Tecno Arte</h2>
-                <p>
-                    En <strong>Innova y Tecno Arte</strong>, podrás explorar el mundo de la <strong>música e iluminación</strong>, 
-                    y el <strong>diseño de artículos de moda</strong>. Descubre el arte del <strong>upcycling</strong> y participa en 
-                    investigaciones para <strong>descubrir nuevas propuestas</strong>.
-                </p>
+                <div className={styles.sectionInfo}>
+                    <h2>Innova y Tecno Arte</h2>
+                    <p>
+                        En <strong>Innova y Tecno Arte</strong>, podrás explorar el mundo de la <strong>música e iluminación</strong>, 
+                        y el <strong>diseño de artículos de moda</strong>. Descubre el arte del <strong>upcycling</strong> y participa en 
+                        investigaciones para <strong>descubrir nuevas propuestas</strong>.
+                    </p>
+                </div>
                 <img src="/tecnoarte.webp" alt="Innova y Tecno Arte" className={styles.image}/>
             </section>
 
-            <section className={styles.section} id='red-de-clubes'>
-                <h2>Red de Clubes</h2>
-                <p>
-                    Con la <strong>Red de Clubes</strong>, te ayudamos a <strong>armar tu club en la escuela o en tu barrio</strong>. 
-                    Nosotros te apoyamos para que puedas crear tu propio espacio de ciencia y tecnología donde quiera que estés.
-                </p>
+            <section className={styles.section2} id='red-de-clubes'>
+                <div className={styles.sectionInfo}>
+                    <h2>Red de Clubes</h2>
+                    <p>
+                        Con la <strong>Red de Clubes</strong>, te ayudamos a <strong>armar tu club en la escuela o en tu barrio</strong>. 
+                        Nosotros te apoyamos para que puedas crear tu propio espacio de ciencia y tecnología donde quiera que estés.
+                    </p>
+                </div>
                 <img src="/red-de-clubes2.webp" alt="Red de Clubes" className={styles.image}/>
             </section>
-            <Link to={"/formulario"} className={styles.ctaButton}>Unirme Ahora</Link>
+            <div className={styles.btnCont}>
+                <Link to={"/formulario"} className={styles.ctaButton}>Unirme Ahora</Link>
+            </div>
         </div>
     );
 };
