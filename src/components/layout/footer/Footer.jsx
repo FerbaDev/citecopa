ec8ffb92df47ea88e42286ff54c9fbf16a8b8f4b
FerbaDev <elferbarron1@gmail.com>
2024-06-28 23:57:49 -0300

    correcciones en footer

--- src/components/layout/footer/Footer.jsx
+++ src/components/layout/footer/Footer.jsx
@@ -1,87 +1,10 @@
-// import { Link } from "react-router-dom";
-// import WhatsAppIcon from "@mui/icons-material/WhatsApp";
-// import InstagramIcon from "@mui/icons-material/Instagram";
-// import ReactWhatsapp from "react-whatsapp";
-// import { Box } from "@mui/material";
-// import "../../../index.css"
-
-// export const Footer = () => {
-//   return (
-//     <>
-//       <Box
-//         sx={{
-//           width: "100%",
-//           minHeight: "300px",
-//           backgroundColor: "#1c1c1c",
-//           color: "whitesmoke",
-//         }}
-//       >
-//         <Box
-//           sx={{
-//             width: "100%",
-//             display: "flex",
-//             flexDirection: "column",
-//             alignItems: "center",
-//             paddingBlock: "20px",
-//           }}
-//         >
-//           <h2 style={{fontFamily: "var(--font-spartan)"}}>
-//             Nos encontrás en:
-//           </h2>
-//           <Box sx={{ display: "flex", flexDirection: "row", gap: "2em" }} p={2}>
-//             <ReactWhatsapp
-//               number="549-2920-35-4587"
-//               message="Hola! Quisiera mas info sobre Citecopa!"
-//               element="div"
-//               className="cursor-manito"
-//             >
-//               <WhatsAppIcon style={{ fontSize: "3em" }} />
-//             </ReactWhatsapp>
-//             <Link
-//               to={"https://www.instagram.com/linkclubdecienciasviedma/"}
-//               style={{ color: "whitesmoke" }}
-//             >
-//               <InstagramIcon style={{ fontSize: "3em" }} />
-//             </Link>
-//           </Box>
-//         </Box>
-//         <Box>
-//           <h2 style={{fontFamily: "var(--font-spartan)", textAlign: "center"}}>
-//             Dirección: Italia 401 ¨b¨.
-//           </h2>
-//           <h3 style={{fontFamily: "var(--font-spartan)", textAlign: "center"}}>
-//             Viedma, Río Negro 
-//           </h3>
-//         </Box>
-//       </Box>
-
-//       {/* footer talba  */}
-//       <Box
-//         sx={{
-//           backgroundColor: "black",
-//           width: "100%",
-//           display: "flex",
-//           justifyContent: "end",
-//           p: 2,
-//           color: "whitesmoke",
-//         }}
-//       >
-//         <p>Tienda desarrollada por</p>
-//           <Link to={"https://talbalabs.com.ar/"} className="talba">
-//             <span style={{ fontFamily: "monospace" }}> Talba Labs</span>
-//           </Link>
-        
-//       </Box>
-//     </>
-//   );
-// };
-
-import { Box, Typography, Container, Grid, Link } from '@mui/material';
-import { Instagram, Facebook, Google} from '@mui/icons-material';
+import { Box, Typography, Container, Grid, Link as MuiLink } from '@mui/material';
+import { Instagram, Facebook, Google } from '@mui/icons-material';
 import ReactWhatsapp from 'react-whatsapp';
 import WhatsAppIcon from "@mui/icons-material/WhatsApp";
+import { Link } from 'react-router-dom';
 
-export const Footer = () => {
+const Footer = () => {
   return (
     <Box sx={{ backgroundColor: '#1c2331', color: 'whitesmoke', fontFamily: "var(--font-spartan)" }}>
       <Box sx={{ backgroundColor: '#6351ce', py: 2 }}>
@@ -91,15 +14,15 @@ export const Footer = () => {
               Encontranos en las redes sociales
             </Typography>
             <Box>
-              <Link href="#" color="inherit" sx={{ mx: 1 }}>
+              <MuiLink href="#" color="inherit" sx={{ mx: 1 }}>
                 <Facebook />
-              </Link>
-              <Link href="#" color="inherit" sx={{ mx: 1 }}>
+              </MuiLink>
+              <MuiLink href="https://www.google.com/maps/place/Italia+401,+R8500API+Viedma,+R%C3%ADo+Negro/@-40.812704,-63.0127384,13.5z/data=!4m5!3m4!1s0x95f69804033feb7b:0x9c548ba25036f745!8m2!3d-40.8122159!4d-63.0078713?hl=es-419&entry=ttu" color="inherit" sx={{ mx: 1 }}>
                 <Google />
-              </Link>
-              <Link href="https://www.instagram.com/linkclubdecienciasviedma/" color="inherit" sx={{ mx: 1 }}>
+              </MuiLink>
+              <MuiLink href="https://www.instagram.com/linkclubdecienciasviedma/" color="inherit" sx={{ mx: 1 }}>
                 <Instagram />
-              </Link>
+              </MuiLink>
             </Box>
           </Box>
         </Container>
@@ -111,8 +34,8 @@ export const Footer = () => {
               Fundación Citecopa
             </Typography>
             <hr style={{ width: '60px', backgroundColor: '#7c4dff', height: '2px', margin: '10px 0' }} />
-            <Typography variant="body1" sx={{fontFamily: "var(--font-spartan)", }}>
-              Here you can use rows and columns to organize your footer content. Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+            <Typography variant="body1" sx={{ fontFamily: "var(--font-spartan)" }}>
+              Aquí puedes usar filas y columnas para organizar el contenido del pie de página. Lorem ipsum dolor sit amet, consectetur adipisicing elit.
             </Typography>
           </Grid>
           <Grid item xs={12} md={2}>
@@ -120,19 +43,19 @@ export const Footer = () => {
               Navegación
             </Typography>
             <hr style={{ width: '60px', backgroundColor: '#7c4dff', height: '2px', margin: '10px 0' }} />
-            <Link href="#!" color="inherit" sx={{ display: 'block', my: 1 }}>Club de Ciencias</Link>
-            <Link href="#!" color="inherit" sx={{ display: 'block', my: 1 }}>Laboratorio</Link>
-            <Link href="#!" color="inherit" sx={{ display: 'block', my: 1 }}>Taller</Link>
-            <Link href="#!" color="inherit" sx={{ display: 'block', my: 1 }}>Liga robótica</Link>
+            <Link to="/club" style={{ display: 'block', margin: '8px 0', color: 'inherit', textDecoration: 'none' }}>Club de Ciencias</Link>
+            <Link to="/laboratorio" style={{ display: 'block', margin: '8px 0', color: 'inherit', textDecoration: 'none' }}>Laboratorio</Link>
+            <Link to="/taller" style={{ display: 'block', margin: '8px 0', color: 'inherit', textDecoration: 'none' }}>Taller</Link>
+            <Link to="/liga-robotica" style={{ display: 'block', margin: '8px 0', color: 'inherit', textDecoration: 'none' }}>Liga robótica</Link>
           </Grid>
           <Grid item xs={12} md={2}>
             <Typography variant="h6" sx={{ fontWeight: 'bold', fontFamily: "var(--font-spartan)", fontSize: "1.5em" }}>
               Links de Interés
             </Typography>
             <hr style={{ width: '60px', backgroundColor: '#7c4dff', height: '2px', margin: '10px 0' }} />
-            <Link href="#!" color="inherit" sx={{ display: 'block', my: 1 }}>Unite al club</Link>
-            <Link href="#!" color="inherit" sx={{ display: 'block', my: 1 }}>Formulario de contacto</Link>
-            <Link href="#!" color="inherit" sx={{ display: 'block', my: 1 }}>Preguntas frecuentes</Link>
+            <Link to="/formulario" style={{ display: 'block', margin: '8px 0', color: 'inherit', textDecoration: 'none' }}>Unite al club</Link>
+            <Link to="/contacto" style={{ display: 'block', margin: '8px 0', color: 'inherit', textDecoration: 'none' }}>Formulario de contacto</Link>
+            <Link to="/contacto#faq" style={{ display: 'block', margin: '8px 0', color: 'inherit', textDecoration: 'none' }}>Preguntas frecuentes</Link>
           </Grid>
           <Grid item xs={12} md={4}>
             <Typography variant="h6" sx={{ fontWeight: 'bold', fontFamily: "var(--font-spartan)", fontSize: "1.5em" }}>
@@ -143,21 +66,21 @@ export const Footer = () => {
             <Typography variant="body1" sx={{ fontFamily: "var(--font-spartan)", fontSize: "1.2em" }}><i className="fas fa-envelope mr-3"></i> info@example.com</Typography>
             <ReactWhatsapp
               number="549-2920-35-4587"
-              message="Hola! Quisiera mas info sobre Citecopa!"
+              message="Hola! Quisiera más información sobre Citecopa!"
               element="div"
-              style={{display: "flex", flexDirection: "row", alignItems: "center", gap: "0.3em", fontSize: "14px"}}
+              style={{ display: 'flex', flexDirection: 'row', alignItems: 'center', gap: '0.3em', fontSize: '14px', cursor: "pointer" }}
             >
-              <WhatsAppIcon />  <p style={{fontFamily: "var(--font-spartan)", fontSize: "1.2em"}}>Whatsapp</p>
+              <WhatsAppIcon /> <p style={{ fontFamily: "var(--font-spartan)", fontSize: "1.2em" }}>Whatsapp</p>
             </ReactWhatsapp>
-            <Typography variant="body1"><i className="fas fa-print mr-3"></i>2920-35-4587</Typography>
+            <Typography variant="body1" sx={{ fontFamily: "var(--font-spartan)", fontSize: "1em" }}><i className="fas fa-print mr-3"></i>2920-35-4587</Typography>
           </Grid>
         </Grid>
       </Container>
       <Box sx={{ backgroundColor: 'rgba(0, 0, 0, 0.2)', py: 2 }}>
         <Container maxWidth="lg">
           <Typography variant="body2" align="center">
-            © 2024 Copyright: 
-            <Link href="#" color="inherit"> Fundación Citecopa</Link>
+            © 2024 Copyright:
+            <Link to="/" style={{ color: 'inherit', textDecoration: 'none' }}> Fundación Citecopa</Link>
           </Typography>
         </Container>
       </Box>
@@ -165,4 +88,4 @@ export const Footer = () => {
   );
 };
 
-
+export default Footer;
